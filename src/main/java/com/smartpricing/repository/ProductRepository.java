b08a9d13b7b148dda9c692298fb7973d01300d0b
lucasgls <lucasglsilva7@gmail.com>
2025-11-13 15:54:34 -0300

    feat: add productRepository with spring Data

--- src/main/java/com/smartpricing/repository/ProductRepository.java
+++ src/main/java/com/smartpricing/repository/ProductRepository.java
@@ -1,11 +1,12 @@
-package repository;
+package com.smartpricing.repository;
 
+import com.smartpricing.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<model.Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
 }
